42900135e236fc98bc3ab565e2adf394fdd745ea
Antonio José Masiá <email@antoniomasia.com>
2023-10-13 23:46:48 +0200

    add nix formatter to config

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -5,6 +5,7 @@ return {
     opts = {
       formatters_by_ft = {
         ["lua"] = { "stylua" },
+        ["nix"] = { "alejandra" },
         ["javascript"] = { { "prettierd", "prettier" } },
         ["javascriptreact"] = { { "prettierd", "prettier" } },
         ["typescript"] = { { "prettierd", "prettier" } },
