e3945f82bd26cec93874c9f9e75c23d658533141
Antonio José Masiá <dev@ajmasia.me>
2023-10-13 15:56:53 +0200

    add new settings
    - config lsp to work fine with lua and stylua, typescript, prettier and eslint
    - add basic lualine customization

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettierd",
+        "eslint-lsp",
+        "shfmt",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      format = {
+        timeout_ms = 500,
+      },
+      servers = {
+        lua_ls = {
+          -- server installed manually behaind stylua
+          mason = false,
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
