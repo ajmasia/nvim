e39edcf644c53713ca13fbebf12a4d2511d44325
Antonio José Masiá <email@antoniomasia.com>
2024-01-02 20:31:47 +0100

    update config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,6 +6,5 @@
 local taskColor = "#d7d7d7"
 local titleColor = "#FFBF00"
 
-vim.cmd('highlight FidgetTitle guifg=' .. titleColor)
-vim.cmd('highlight FidgetTask guifg=' .. taskColor)
-
+vim.cmd("highlight FidgetTitle guifg=" .. titleColor)
+vim.cmd("highlight FidgetTask guifg=" .. taskColor)
